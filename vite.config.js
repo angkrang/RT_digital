5b8f2d0d3a1c97394160e4545c5a765112892018
angkrang <ash.gio87@gmail.com>
2026-08-11 15:02:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import react from "@vitejs/plugin-react";
 // Kalau memakai custom domain, ganti base menjadi "/".
 export default defineConfig({
   plugins: [react()],
-  base: "/RT-digital/",
+  base: "/RT_digital/",
 });
